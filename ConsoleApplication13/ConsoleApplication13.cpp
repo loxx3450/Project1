6cff86ac77877ea09accaeac7af6f158feaee6f2
Yehor Kliushyn <egorka231231@gmail.com>
2022-11-20 18:45:32 +0200

    3. mods

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -3,6 +3,12 @@
 #include <conio.h>
 using namespace std;
 
+struct Mods {
+    int count{ 0 };
+    int countBombs{ 0 };
+    string strip{ "no_title" };
+};
+
 
 enum {
     EMPTY = 0,
@@ -50,9 +56,9 @@ int random(int min = 0, int max = 9) {
     return rand() % (max - min + 1) + min;
 }
 
-void GenerateBombs(int** arr, int count) {
+void GenerateBombs(int** arr, int count, int countBombs) {
     int x{}, y{};
-    for (int i{1}; i <= count; i++) {
+    for (int i{1}; i <= countBombs; i++) {
         x = random(1, count);
         y = random(1, count);
         cout << x << y << endl;
@@ -89,15 +95,18 @@ void BombsAround(int** arr, int count) {
     }
 }
 
-void GenerateArr(int** arr, int count) {
-    GenerateBombs(arr, count);
+void GenerateArr(int** arr, int count, int countBombs) {
+    GenerateBombs(arr, count, countBombs);
     BombsAround(arr, count);
 }
 
-void showArr(int** arr, int** ghost_massiv, int count) {
+void showArr(int** arr, Mods* mods, int mode, int** ghost_massiv, int count) {
+    cout << mods[mode].strip;
     for (int i{1}; i <= count; i++) {
+        cout << "|";
         for (int j{1}; j <= count; j++) {
             if (arr[i][j] == 0) {
+                ghost_massiv[i][j] = 1;
                 cout << "  ";
             }
             else if (ghost_massiv[i][j] == 0) {
@@ -110,8 +119,10 @@ void showArr(int** arr, int** ghost_massiv, int count) {
                 cout << arr[i][j] << " ";
             }
         }
+        cout << "|";
         cout << endl;
     }
+    cout << mods[mode].strip;
 }
 
 void show(int** arr, int count) {
@@ -124,6 +135,8 @@ void show(int** arr, int count) {
 }
 
 bool Shoot(int** arr, int** ghost_massiv, int x, int y) {
+    x--;
+    y--;
     if (x != 0) {
         x /= 2;
     }
@@ -135,22 +148,58 @@ bool Shoot(int** arr, int** ghost_massiv, int x, int y) {
 }
 
 void Flag(int** ghost_massiv, int x, int y) {
+    x--;
+    y--;
     if (x != 0) {
         x /= 2;
     }
     ghost_massiv[y + 1][x + 1] = 2;
 }
+int CheckAround(int** arr, int count, int x, int y) {
+    for (int i{ x - 1 }; i <= x + 1; i++) {
+        for (int j{ y - 1 }; j <= y + 1; j++) {
+            if (arr[i][j] == EMPTY && i >= 1 && i <= count && j >= 1 && j <= count) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void GenerateghostArr(int** arr, int** ghost_massiv, int count) {
+    for (int i{ 1 }; i <= count; i++) {
+        for (int j{ 1 }; j <= count; j++) {
+            ghost_massiv[i][j] = CheckAround(arr, count, i, j);
+        }
+    }
+}
+
+int ifWin(int** ghost_massiv, int count, int countBombs) {
+    int value{ 0 };
+    for (int i{ 1 }; i <= count; i++) {
+        for (int j{ 1 }; j <= count; j++) {
+            if (ghost_massiv[i][j] != 1) {
+                value++;
+            }
+        }
+    }
+    if (value == countBombs) {
+        cout << "You win";
+        return 1;
+    }
+
+    return 0;
+}
 
 
 
-bool play(int** arr, int count) {
+bool play(int** arr, Mods* mods, int mode, int count, int countBombs) {
     int** ghost_massiv = CreateArr(count);
-    int move{}, x{ 0 }, y{ 0 };
+    GenerateghostArr(arr, ghost_massiv, count);
+    int move{}, x{ 1 }, y{ 1 };
 
     system("cls");
-    showArr(arr, ghost_massiv, count);
-    cout << "\n\n";
-    show(arr, count);
+    showArr(arr, mods, mode, ghost_massiv, count);
     
     while (true) {
         move = _getch();
@@ -169,20 +218,23 @@ bool play(int** arr, int count) {
             break;
         case shoot:
             if (!Shoot(arr, ghost_massiv, x, y)) {
-                return 1;
+                system("cls");
+                showArr(arr, mods, mode, ghost_massiv, count);
+                return 0;
             }
             system("cls");
-            showArr(arr, ghost_massiv, count);
-            cout << "\n\n";
-            show(arr, count);
+            showArr(arr, mods, mode, ghost_massiv, count);
             break;
         case flag:
             Flag(ghost_massiv, x, y);
             system("cls");
-            showArr(arr, ghost_massiv, count);
-            cout << "\n\n";
-            show(arr, count);
+            showArr(arr, mods, mode, ghost_massiv, count);
             break;
+        }
+        if (ifWin(ghost_massiv, count, countBombs)) {
+            system("cls");
+            showArr(arr, mods, mode, ghost_massiv, count);
+            return 1;
         }
         
         GotoXY(x, y);
@@ -190,20 +242,55 @@ bool play(int** arr, int count) {
 
 }
 
+int Choice() {
+    cout << "Choose game difficulty: \n1.Easy \n2.Normal \n3.Hard \n";
+    int move{}, x{ 0 }, y{ 0 };
+
+    while (true) {
+        move = _getch();
+        switch (move) {
+        case up:
+            y--;
+            break;
+        case down:
+            y++;
+            break;
+        case shoot:
+            return y - 1;
+            break;
+
+            
+        }
+        GotoXY(x, y);
+    }
+}
+
 int main()
 {
     srand(time(NULL));
     rand();
 
-    int count{ 8 };
+    Mods* mods = new Mods[3]{
+        {10, 10, "----------------------\n"},
+        {15, 20, "--------------------------------\n"},
+        {20, 40, "------------------------------------------\n"}
+    };
+
+    int mode = Choice();
+
+    const int count = mods[mode].count;
+
+    const int countBombs = mods[mode].countBombs;
     
     int** arr = CreateArr(count);
 
-    GenerateArr(arr, count);
+    GenerateArr(arr, count, countBombs);
 
-    if (play(arr, count)) {
-        system("cls");
-        cout << "You losed";
+    if (play(arr, mods, mode, count, countBombs)) {
+        cout << "You Won";
+    }
+    else {
+        cout << "You Losed";
     }
 
     return 0;
